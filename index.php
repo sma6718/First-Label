6313ab7795ed4bdcf813fb02b24b81a1addeff8c
unknown <sma6718@stacmail.net>
2020-06-21 16:28:25 +1200

    21/06/2020

--- index.php
+++ index.php
@@ -3,68 +3,60 @@
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link rel="stylesheet" href="styles.css">
-
     <title>First Label</title>
   </head>
-  <!-- Start of the body tag-->
   <body>
-    <!-- Column that wraps around all content -->
-    <div class="col">
-      <!-- Navbar row -->
-      <div class="row p-1" style="border-bottom:solid; border-color:grey; border-width: thin;">
-        <!-- Logo Column within Navbar -->
-        <div class="col-xl-2 mt-2 mb-2" style="border-right:solid; border-color:grey; border-width: thin;">
-          <!-- First Label logo -->
-          <div class="row">
-            <div class="col logo">
-              <img class="logo-image" src="images/FirstLabel.png" alt="First Label Logo">
-            </div>
-          </div>
-          <div class="row pl-3">
-            <h5 style="margin-left: 3%; opacity:95%;">More than meets the eye</h5>
-          </div>
-
-        <!-- End of Logo Column within Navbar -->
+    <div class="header">
+      <div class="logo-area">
+        <div class="logo">
+          <img src="images/FirstLabel.png" alt="First Label logo">
         </div>
-        <!-- Column with navbar links -->
-        <div class="col-xl-6 ml-4 mt-1 mb-1 ">
-          <div class="row pt-4">
-            <div class="col-xl-3 navlink">
-              <h2>Home</h2>
-            </div>
-            <div class="col-xl-4">
-              <h2>Info for Designers</h2>
-            </div>
-            <div class="col-xl-5">
-              <h2>Request a Free Quote</h2>
-            </div>
-          </div>
-        <!-- End of column with navbar links-->
-        </div>
-        <!-- Searchbar and ph column-->
-        <div class="col-xl-2">
-          <h1>3</h1>
-        <!-- End of column with searchbar and ph-->
-        </div>
-        <!-- Column with NZ Image -->
-        <div class="col-xl-1">
-          <h1>4</h1>
-        <!-- End of column with NZ Image -->
+        <div class="title">
+          <h4>More than meets the eye</h4>
         </div>
       </div>
-      <!-- End of Navbar-->
+      <div class="navbar-area">
+        <a href="#">Home</a>
+        <a href="#">Info for Designers</a>
+        <a href="#">Request a Free Quote</a>
+      </div>
+      <div class="NZ">
+        <img src="images/nz.png" alt="nz">
+      </div>
+      <div class="searchbar-area">
+        <div class="searchbar">
+          <div class="topnav">
+            <div class="search-container">
+              <form action="/action_page.php">
+                <input type="text" placeholder="Search.." name="search">
+                <button type="submit"><i class="fa fa-search"></i></button>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div class="ph">
+          <p>(03) 349 6633</p>
+        </div>
+      </div>
     </div>
-    <!-- End of main column-->
+    <div class="body">
+
+    </div>
+    <div class="footer">
+
+    </div>
+
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-  <!-- End of Body tag -->
+
   </body>
 </html>
